58f4dbc0b5d8b17aea8f4e1d8c445e32199b0574
shivam <@shankyadhar.com>
2022-11-14 23:07:08 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return(
+       <div className='App'>
+           <h1>
+              About Me
+           </h1>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi voluptas, et ex praesentium deleniti ipsa aut quisquam rerum ad magnam, repellendus, a vero alias libero magni id accusamus nihil repudiandae!</p>
+           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis nostrum, quod, animi tempore placeat in alias, qui asperiores earum ipsam consequatur harum totam facere ducimus autem illo assumenda. At porro temporibus provident vitae necessitatibus, esse impedit, delectus itaque unde autem, quae aperiam odit est vero? Nam earum excepturi cupiditate asperiores, consectetur fugiat dolorem, rerum aspernatur, numquam ipsa molestiae odit voluptatibus. Assumenda minima sunt illum dignissimos, odio ut. Iure neque id magni quisquam, suscipit dicta ullam numquam nulla debitis accusantium quia dolore voluptatem officia. Ducimus consequuntur, ea voluptatum minus iure commodi repellendus, doloribus asperiores esse, enim laboriosam id natus tempora dignissimos?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi vitae temporibus molestiae quisquam dolor vero sunt facilis. Quasi laborum omnis possimus fuga repellendus laboriosam, assumenda voluptas provident modi ullam esse iusto, doloremque rerum vel beatae at ratione. Doloremque recusandae placeat, natus asperiores adipisci excepturi praesentium necessitatibus ducimus atque iste aut libero ipsam sapiente quasi vel nostrum explicabo est? Quas, id!</p>
+       </div>
+   )
 }
 
 export default App;
